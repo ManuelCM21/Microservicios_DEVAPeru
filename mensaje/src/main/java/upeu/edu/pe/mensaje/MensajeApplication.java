72e2f06a57a54b59c92264897fcd8b45c2697c36
10419 <martinhilasaca7@gmail.com>
2023-05-17 16:54:02 -0600

    microservicio-mensaje

---
+++ mensaje/src/main/java/upeu/edu/pe/mensaje/MensajeApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.mensaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensajeApplication.class, args);
+	}
+
+}
