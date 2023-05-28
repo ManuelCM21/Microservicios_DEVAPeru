741f6a73e9a9910f4b42e343c1c1191694188520
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-05-28 10:09:34 -0500

    .

--- venta/src/main/java/upeu/edu/pe/venta/Repository/VentaRepository.java
+++ venta/src/main/java/upeu/edu/pe/venta/Repository/VentaRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import upeu.edu.pe.venta.entity.Venta;
 
 public interface VentaRepository extends JpaRepository<Venta,Integer>{
-    public List<Venta> findByCustomerId(Integer ventaid );
+    public List<Venta> findByVentaId(Integer ventaid );
     public Venta findBySerie(String serie);
 }
