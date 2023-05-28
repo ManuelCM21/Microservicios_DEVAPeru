67507c6b1827c530ef4c87ba374885f3cf62b9f8
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-05-28 10:25:33 -0500

    update

--- venta/src/main/java/upeu/edu/pe/venta/feing/ProductoFeing.java
+++ venta/src/main/java/upeu/edu/pe/venta/feing/ProductoFeing.java
@@ -4,14 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import upeu.edu.pe.venta.dto.Producto;
 
 
-@FeignClient(name = "catalogo-service")
-@RequestMapping(value = "/producto")
+@FeignClient(name = "catalogo-service",path = "/producto")
+
 public interface ProductoFeing {
     @GetMapping("/{id}")
     public ResponseEntity<Producto> getProducto(@PathVariable(required = true) Integer id) ;
