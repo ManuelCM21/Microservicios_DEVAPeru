d33cf790c0aabaa8057dfd5a299ea2838bfcec5a
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-05-28 09:48:54 -0500

    .

--- venta/src/main/java/upeu/edu/pe/venta/VentaApplication.java
+++ venta/src/main/java/upeu/edu/pe/venta/VentaApplication.java
@@ -3,9 +3,11 @@ package upeu.edu.pe.venta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableFeignClients
+@Component
 public class VentaApplication {
 
 	public static void main(String[] args) {
