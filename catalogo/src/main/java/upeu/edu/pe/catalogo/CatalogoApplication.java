f76b7f4eab953f9bbdb41c79649c828649156785
Frank8040 <frank.chunca@upeu.edu.pe>
2023-06-14 18:25:09 -0500

    Modificado

--- catalogo/src/main/java/upeu/edu/pe/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/upeu/edu/pe/catalogo/CatalogoApplication.java
@@ -2,13 +2,13 @@ package upeu.edu.pe.catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatalogoApplication implements WebMvcConfigurer {
