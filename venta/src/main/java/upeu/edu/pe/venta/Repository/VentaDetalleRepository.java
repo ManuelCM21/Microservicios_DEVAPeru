64e6e158d14b74c946936b83bd7e62e8f3016e47
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-06-18 20:53:34 -0500

    eliminando

--- venta/src/main/java/upeu/edu/pe/venta/Repository/VentaDetalleRepository.java
+++
@@ -1,9 +0,0 @@
-package upeu.edu.pe.venta.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import upeu.edu.pe.venta.entity.VentaDetalle;
-
-public interface VentaDetalleRepository extends JpaRepository<VentaDetalle, Integer> {
-    
-}
